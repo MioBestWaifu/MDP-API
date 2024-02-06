2778b25c1c3d2e1651e8ce11bd10a635adbd4caf
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-06 20:15:11 -0300

    Create PagesController.cs

---
+++ MDP/Controllers/PagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDP.Controllers
+{
+    [ApiController]
+    [Route("pages")]
+    public class PagesController : ControllerBase
+    {
+        [HttpGet(Name = "GetPages")]
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
