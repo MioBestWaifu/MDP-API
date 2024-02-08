b59f1922465fc55cbbf8f147996e5982e67625f1
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-08 18:23:15 -0300

    Preparer de named properties

--- MDP/Data/StatementPreparer.cs
+++ MDP/Data/StatementPreparer.cs
@@ -1,6 +1,121 @@
-﻿namespace MDP.Data
+﻿using MySql.Data.MySqlClient;
+
+namespace MDP.Data
 {
     public class StatementPreparer
     {
+        public static MySqlCommand GetCountry(int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getCountry);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand GetDemographics(int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getDemographics);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand GetCategory (int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getCategory);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+
+        public static MySqlCommand GetAgeRating (int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getAgeRating);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand GetImageType (int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getImageType);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand GetMedia (int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getMedia);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand GetRole (int id)
+        {
+            MySqlCommand com = new MySqlCommand(Statements.getRole);
+            com.Parameters.AddWithValue("@id", id);
+            return com;
+        }
+
+        public static MySqlCommand
+        {
+
+        }
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
+
+
+        public static MySqlCommand
+        {
+
+        }
     }
 }
