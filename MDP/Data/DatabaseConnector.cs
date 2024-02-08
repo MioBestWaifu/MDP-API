dae43c065f45b0a8a9a224820387c15d972e35c0
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-08 13:10:33 -0300

    Construçãõ de DatabaseConnector

--- MDP/Data/DatabaseConnector.cs
+++ MDP/Data/DatabaseConnector.cs
@@ -2,5 +2,27 @@
 {
     public class DatabaseConnector
     {
+        private string connectionString;
+
+        public DatabaseConnector(IWebHostEnvironment environment, ILogger<DatabaseConnector> logger)
+        {
+            if (environment.IsDevelopment())
+            {
+                string filePath = Path.Combine(environment.ContentRootPath, "connection.txt");
+                try
+                {
+                    connectionString = File.ReadAllText(filePath);
+                } catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException){
+                    logger.LogInformation("\nconnection.txt, que deveria estar no root da aplicação (/MDP), não foi encontrado\nO seguinte caminho foi tentado: " +
+                        filePath);
+                    throw;
+                }
+            }
+            else
+            {
+                connectionString = "Server=prod;Database=prod;User Id=prod;Password=prod;";
+            }
+            logger.LogInformation("\n"+connectionString+"\n");
+        }
     }
 }
