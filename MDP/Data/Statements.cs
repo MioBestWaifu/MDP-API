671daa86cb45668e0794fae2ed4241f63c7d134b
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-08 17:05:47 -0300

    Statements de namedProperties

--- MDP/Data/Statements.cs
+++ MDP/Data/Statements.cs
@@ -2,6 +2,14 @@
 {
     public class Statements
     {
+        public static string getCountry = "SELECT name, code FROM countries WHERE id = @id",
+            getDemographics = "SELECT name FROM demographics WHERE id = @id",
+            getCategory = "SELECT name FROM categories WHERE id = @id",
+            getAgeRating = "SELECT name FROM agerating WHERE id = @id",
+            getImageType = "SELECT name FROM imagetypes WHERE id = @id",
+            getMedia = "SELECT name FROM medias WHERE id = @id",
+            getRole = "SELECT name FROM roles WHERE id = @id";
+
         public static string getWorkById = "SELECT * FROM works WHERE id = @work", 
             getWorkCategoriesByWorkId = "SELECT name FROM categories WHERE id IN (SELECT category FROM workcategories WHERE work = @work)",
             getWorkOtherNames = "SELECT name FROM workothernames WHERE work = @work",
