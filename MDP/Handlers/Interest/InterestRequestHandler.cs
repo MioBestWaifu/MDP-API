71c5ea315774672cd561f49968087c7624a54fec
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-09 13:06:40 -0300

    Implementando InterestRequestHandler

--- MDP/Handlers/Interest/InterestRequestHandler.cs
+++ MDP/Handlers/Interest/InterestRequestHandler.cs
@@ -1,6 +1,41 @@
 ﻿namespace MDP.Handlers.Interest
 {
-    public class InterestRequestHandler
+    using MDP.Models;
+    using MDP.Data;
+    using MySql.Data.MySqlClient;
+    using System.Threading.Tasks;
+
+    public class InterestRequestHandler : IRequestHandler<Interest>
     {
+        private DatabaseConnector connector;
+
+        public InterestRequestHandler(DatabaseConnector connector)
+        {
+            this.connector = connector;
+        }
+
+        public async Task<Interest> HandleRequest(int id)
+        {
+            Task<MySqlDataReader> interestTask = connector.ExecuteQuery(StatementPreparer.GetInterestById(id));
+            Task<MySqlDataReader> demographicsTask = GetDemographics(id);
+
+            MySqlDataReader interestReader = await interestTask;
+            interestReader.Read();
+            Interest toReturn = Interest.FromQuery(interestReader);
+
+            Task? demographicsAfterTask = demographicsTask.ContinueWith((task) =>
+            {
+                toReturn.SetDemographics(task.Result);
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            Task.WaitAll(demographicsAfterTask);
+
+            return toReturn;
+        }
+
+        private async Task<MySqlDataReader> GetDemographics(int id)
+        {
+            return await connector.ExecuteQuery(StatementPreparer.GetInterestDemographicsByInterestId(id));
+        }
     }
 }
