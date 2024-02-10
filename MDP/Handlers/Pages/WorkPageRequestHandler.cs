775d7e5983d45ebc98ba8a5d1b66e39a59ea8cfe
MioBestWaifu <Fenix1603@hotmail.com>
2024-02-10 00:07:30 -0300

    Implementando WorkPageRequestHandler

--- MDP/Handlers/Pages/WorkPageRequestHandler.cs
+++ MDP/Handlers/Pages/WorkPageRequestHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace MDP.Handlers.Pages
+﻿
+
+namespace MDP.Handlers.Pages
 {
-    public class WorkPageRequestHandler
+    using MDP.Data;
+    using MDP.Handlers.Work;
+    using MDP.Handlers.Link;
+    using MDP.Models.Artifacts;
+    using MDP.Models.Pages;
+    using MDP.Models;
+    public class WorkPageRequestHandler(DatabaseConnector conn) : Handler(conn), IRequestHandler<WorkPageModel>
     {
+        /// <summary>
+        /// Falta adicionar reviews, related works, news and highlights e fractions
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<WorkPageModel> HandleRequest(int id)
+        {
+            Task<Artifact> workTask = new WorkRequestHandler(conn).HandleRequest(id);
+            Task<List<Link>> personsTask = new ParticipantPersonsAsLinksRequestHandler(conn).HandleRequest(id);
+            Task<List<Link>> companiesTask = new ParticipantCompaniesAsLinksRequestHandler(conn).HandleRequest(id);
+
+            Artifact artifact = await workTask;
+            WorkPageModel toReturn = new WorkPageModel();
+            toReturn.Participants = [.. await companiesTask,.. await personsTask];
+            toReturn.Work = artifact;
+            return toReturn;
+        }
     }
 }
