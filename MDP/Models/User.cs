88b1d6264b768c4b539e3c2084398a12674e4838
MioBestWaifu <Fenix1603@hotmail.com>
2024-07-24 14:39:19 -0300

    Adapting User

--- MDP/Models/User.cs
+++ MDP/Models/User.cs
@@ -9,14 +9,14 @@
         public int Id { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public string Nickname { get; set; }
-        public string CardImgUrl { get; set; }
-        public string MainImgUrl { get; set; }
-        public List<string> OtherImgUrls { get; set; }
-        public string Country { get; set; }
+        public Name Nickname { get; set; }
+        public Image CardImageUrl { get; set; }
+        public Image MainImageUrl { get; set; }
+        public List<Image>? OtherImageUrls { get; set; }
+        public Country? Country { get; set; }
         public string Description { get; set; }
         public DateTime Birthday { get; set; }
-        public string Gender { get; set; }
+        public Gender Gender { get; set; }
 
         /// <summary>
         /// Cria um user com id, email, password, nickname, description e birthday.
