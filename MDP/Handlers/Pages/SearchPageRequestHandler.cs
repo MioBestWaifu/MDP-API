e258b6ae99b5aba4ef1ad56e416c872308b09dbf
MioBestWaifu <Fenix1603@hotmail.com>
2024-07-25 15:34:15 -0300

    Filling SearchPage

--- MDP/Handlers/Pages/SearchPageRequestHandler.cs
+++ MDP/Handlers/Pages/SearchPageRequestHandler.cs
@@ -2,6 +2,7 @@
 using MDP.Models.Pages;
 using MDP.Models.Works;
 using MDP.Utils;
+using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 
 namespace MDP.Handlers.Pages
@@ -11,9 +12,12 @@ namespace MDP.Handlers.Pages
         public async Task<SearchPageModel> HandleSearch(string query, int page = 0)
         {
             List<Artifact> artifacts = connector.Artifacts
-        .Where(a => a.ShortName.Literal.Contains(query) && a.FullName.Literal.Contains(query))
-        .Skip(page * Constants.MAX_SEARCH_WORKS)
-        .Take(Constants.MAX_SEARCH_WORKS).ToList();
+                .Where(a => a.ShortName.Literal.Contains(query) && a.FullName.Literal.Contains(query))
+                .Skip(page * Constants.MAX_SEARCH_WORKS)
+                .Include(a => a.ShortName)
+                .Include(a => a.FullName)
+                .Include(a => a.CardImage)
+                .Take(Constants.MAX_SEARCH_WORKS).ToList();
 
             SearchPageModel toReturn = new SearchPageModel
             {
