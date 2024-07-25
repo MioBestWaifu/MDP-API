9c3b19ebe0ddbf6830d59e6add0a354b6da8b272
MioBestWaifu <Fenix1603@hotmail.com>
2024-07-25 15:14:21 -0300

    Filling HomePage request

--- MDP/Handlers/Pages/HomePageRequestHandler.cs
+++ MDP/Handlers/Pages/HomePageRequestHandler.cs
@@ -4,6 +4,7 @@ using MDP.Models;
 using MDP.Models.Pages;
 using MDP.Models.Works;
 using MDP.Utils;
+using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 
 namespace MDP.Handlers.Pages
@@ -13,8 +14,24 @@ namespace MDP.Handlers.Pages
         public async Task<HomePageModel> HandleRequest(int id)
         {
             HomePageModel toReturn = new HomePageModel();
-            toReturn.Artifacts = connector.Artifacts.Take(10).ToList();
-            toReturn.NewsAndHighlights = connector.GlobalNews.OrderByDescending(x=>x.News.Date).Take(Constants.MAX_RECENT_NEWS).Select(x=> x.News).ToList();
+            toReturn.Artifacts = connector.Artifacts
+                .Include(a => a.Categories)
+                .Include(a => a.TargetDemographics)
+                .Include(a => a.AgeRating)
+                .Include(a => a.CardImage)
+                .Include(a => a.MainImage)
+                .Include(a => a.Media)
+                .Include(a => a.ShortName)
+                .Include(a => a.FullName)
+                .Take(10)
+                .ToList();
+
+            toReturn.NewsAndHighlights = connector.GlobalNews
+                .OrderByDescending(x=>x.News.Date)
+                .Take(Constants.MAX_RECENT_NEWS)
+                .Select(x=> x.News)
+                .ToList();
+
             return toReturn;
         }
     }
